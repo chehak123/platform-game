6fc06149220ba0ccb0a121017f39eda72ef17e24
chehak123 <chehakagrawal01@gmail.com>
2020-03-29 17:41:48 +0530

    first commit

---
+++ Assets/playercontrol.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour
+{
+
+    private Rigidbody2D myrigidbody;
+    
+    private Animator myanimator;
+    [SerializeField]
+    private float movtspeed;
+    private bool facingright;
+    
+
+    void Start()
+    {
+        facingright=true;                                               //initially he will always be facing towards right 
+        myrigidbody=GetComponent<Rigidbody2D>();                       // helpS to develop relation between rigidbody defined in this and unity hub
+        myanimator=GetComponent<Animator>();                         // develop relation between animator in unity hub nd myanimator
+    }
+
+    private void handlemovt(float horizontal)                           //function
+    {                                        
+        myrigidbody.velocity= new Vector2(horizontal*movtspeed ,myrigidbody.velocity.y);
+        myanimator.SetFloat("speed",Mathf.Abs(horizontal));                 
+    }
+
+    private void flip(float horizontal)
+    {
+         if(horizontal>0 && !facingright || horizontal<0 && facingright)
+         {
+            facingright=!facingright;
+            Vector3 thescale=transform.localScale;           //access to player localscale
+            thescale.x*=-1;                                  //we have multiplied the x value of scale with -1 to change the direction
+            transform.localScale=thescale;
+         }
+    }
+
+    void FixedUpdate()                                                                      //main function
+    {
+        float horizontal= Input.GetAxis("Horizontal");
+        handlemovt(horizontal);
+        flip(horizontal);
+    }
+}
---
+++ Assets/scripts/sc.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc : MonoBehaviour
+{
+    private Rigidbody2D myrigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       myrigidbody=GetComponent<Rigidbody2D>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal= Input.GetAxis("Horizontal");
+      
+        Handlemovement(horizontal);
+    }
+
+
+    private void Handlemovement(float horizontal){
+
+        myrigidbody.velocity= new Vector2(horizontal,myrigidbody.velocity.y);
+    }
+}
+
+
